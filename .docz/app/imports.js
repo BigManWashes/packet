b6b39cd0d79995d0689bda2347715c4bda380831
Adam Grant <writeadamg@gmail.com>
2019-03-08 06:57:57 -0600

    Added controls portion

--- .docz/app/imports.js
+++ .docz/app/imports.js
@@ -1,4 +1,6 @@
 export const imports = {
   'mdx/home.mdx': () =>
     import(/* webpackPrefetch: true, webpackChunkName: "mdx-home" */ 'mdx/home.mdx'),
+  'mdx/Controls/controls.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "mdx-controls-controls" */ 'mdx/Controls/controls.mdx'),
 }
